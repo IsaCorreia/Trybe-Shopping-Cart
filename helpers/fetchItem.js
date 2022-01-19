c325b3db738f6a6072412a9fc677ae03f00f00f4
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-19 16:05:13 -0300

    Req 2 - parcial: implementando funçao fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,16 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetch = require('node-fetch');
+
+const fetchItem = (input) => {
+if (input === undefined) throw new Error('You need to provide an item ID');
+const url = `https://api.mercadolibre.com/items/${input}`;
+
+  try {
+    return fetch(url)
+    .then((response) => response.json())
+    .then((response) => response);
+  } catch (error) {
+    throw new Error(`Erro: ${error}`);
+  }
 };
 
 if (typeof module !== 'undefined') {
