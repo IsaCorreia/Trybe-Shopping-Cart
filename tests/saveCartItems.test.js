f1c5b1d9292907f8fdb1abd452619155001eb47f
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-22 20:42:05 -0300

    Requisito 10

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,14 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se provido com o elemento HTML, chama o método localStorage', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Verifica se provido com o elemento HTML, chama o método localStorage, com dois argumentos', () => {
+    const elHTML = '<ol><li>Item</li></ol>';
+    saveCartItems(elHTML);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', elHTML);
+  })
 });
